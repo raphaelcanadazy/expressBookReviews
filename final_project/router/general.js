a97ff31f8cfdf42d70ad70ac42f48236fc299ad2
raphaelcanadazy <raphaelcanadazy@gmail.com>
2025-02-14 07:38:10 -0500

    update general 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,7 +30,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const authorInBooks = [];
+  for(let book in books){
+    if(books[book].author === author){
+        authorInBooks.push(books[book]);
+    }
+  }
+
+  if(authorInBooks.length > 0){
+    return res.status(300).json(authorInBooks);
+  }else{
+    return res.status(404).json({ message: "Author not found."});
+  }
 });
 
 // Get all books based on title
