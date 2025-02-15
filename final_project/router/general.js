f95a16c274f943dc154721f5c8fa262e8ccfead8
raphaelcanadazy <raphaelcanadazy@gmail.com>
2025-02-15 17:19:02 -0500

    update 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,12 +24,31 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
+/* For verify task10
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
   return res.status(300).json(books);
 });
+*/
 
+// Task 10
+function getBooks() {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+}
+public_users.get('/', async function (req, res) {
+    try {
+      const bks = await getBooks();
+      res.send(JSON.stringify(bks, null, 4));
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+});
+
+/* For verify task11
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
@@ -40,7 +59,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.status(404).json({ message: "Book not found."});
   }
  });
-  
+ */
+
+function getByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+        let isbnNum = parseInt(isbn);
+        if (books[isbnNum]) {
+            resolve(books[isbnNum]);
+        } else {
+            reject({status:404, message:`ISBN ${isbn} not found`});
+        }
+    })
+}
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+    getByISBN(req.params.isbn)
+    .then(
+        result => res.send(result),
+        error => res.status(error.status).json({message: error.message})
+    );
+});
+
+/* For verify task 12
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
@@ -58,7 +98,18 @@ public_users.get('/author/:author',function (req, res) {
     return res.status(404).json({ message: "Author not found."});
   }
 });
+*/
 
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    const author = req.params.author;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.author === author))
+    .then((filteredBooks) => res.send(filteredBooks));
+});
+
+/* For verify task 13
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
@@ -75,6 +126,16 @@ public_users.get('/title/:title',function (req, res) {
   }else{
     return res.status(404).json({ message: "Title not found."});
   }
+});
+*/
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+    const title = req.params.title;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.title === title))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
 //  Get book review
